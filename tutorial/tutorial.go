4827ba59b5dca1d4756b79747f59ef05ee48f423
Christopher Wood <caw@heapingbits.net>
2023-09-15 15:10:13 -0400

    Refactor tutorial code for readability

--- tutorial/tutorial.go
+++ tutorial/tutorial.go
@@ -9,24 +9,44 @@ import (
 	"example.com/util"
 )
 
-var DBSize uint64
-var ChunkSize uint64
-var ChunkNum uint64
-var DB []uint64
+type Server struct {
+	DB        []uint64
+	DBSize    uint64
+	ChunkSize uint64
+	ChunkNum  uint64
+}
 
-func PossibleParities(offsetVec []uint64) []uint64 {
+type Client struct {
+	DBSize    uint64
+	ChunkSize uint64
+	ChunkNum  uint64
+
+	Q  uint64
+	M1 uint64
+	M2 uint64
+
+	primaryHints []LocalHint
+	backupHints  []LocalHint
+}
+
+func (s Server) possibleParities(offsetVec []uint64) []uint64 {
 	// Run by the server. Given a punctured offset, it first guesses the position of the punctured entry,
 	// then it computes the possible parities.
-	parities := make([]uint64, ChunkNum)
+	parities := make([]uint64, s.ChunkNum)
 	parities[0] = 0
-	for i := uint64(0); i < ChunkNum-1; i++ {
-		xi := (i+1)*ChunkSize + offsetVec[i]
-		parities[0] ^= DB[xi]
+	for i := uint64(0); i < s.ChunkNum-1; i++ {
+		xi := (i+1)*s.ChunkSize + offsetVec[i]
+		parities[0] ^= s.DB[xi]
 	}
-	for i := uint64(0); i < ChunkNum-1; i++ {
-		parities[i+1] = parities[i] ^ DB[(i+1)*ChunkSize+offsetVec[i]] ^ DB[i*ChunkSize+offsetVec[i]]
+	for i := uint64(0); i < s.ChunkNum-1; i++ {
+		parities[i+1] = parities[i] ^ s.DB[(i+1)*s.ChunkSize+offsetVec[i]] ^ s.DB[i*s.ChunkSize+offsetVec[i]]
 	}
 	return parities
+}
+
+// XXX(caw): this should be extended to support pipelining for unbounded queries
+func (s Server) Process(offsetVec []uint64) []uint64 {
+	return s.possibleParities(offsetVec)
 }
 
 type LocalHint struct {
@@ -37,117 +57,182 @@ type LocalHint struct {
 }
 
 // Elem returns the element in the chunkID-th chunk of the hint. It takes care of the case when the hint is programmed.
-func Elem(hint *LocalHint, chunkId uint64) uint64 {
-	if hint.isProgrammed && chunkId == hint.programmedPoint/ChunkSize {
+func (c Client) Elem(hint *LocalHint, chunkId uint64) uint64 {
+	if hint.isProgrammed && chunkId == hint.programmedPoint/c.ChunkSize {
 		return hint.programmedPoint
 	} else {
-		return util.PRFEval(&hint.key, chunkId)%ChunkSize + chunkId*ChunkSize
+		return util.PRFEval(&hint.key, chunkId)%c.ChunkSize + chunkId*c.ChunkSize
 	}
 }
 
-func PIR() {
-	// Suppose there's a public DB.
-	DB = make([]uint64, DBSize)
+func NewServer(DBSize uint64) Server {
+	DB := make([]uint64, DBSize)
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < int(DBSize); i++ {
+	for i := uint64(0); i < DBSize; i++ {
 		DB[i] = rng.Uint64()
 	}
 
 	// setup the parameters
-	ChunkSize = uint64(math.Sqrt(float64(DBSize)))
-	ChunkNum = uint64(math.Ceil(float64(DBSize) / float64(ChunkSize)))
-	log.Printf("DBSize: %d, ChunkSize: %d, ChunkNum: %d", DBSize, ChunkSize, ChunkNum)
+	ChunkSize := uint64(math.Sqrt(float64(DBSize)))
+	ChunkNum := uint64(math.Ceil(float64(DBSize) / float64(ChunkSize)))
 
-	// The following is the client side algorithm.
-	Q := uint64(math.Sqrt(float64(DBSize)) * math.Log(float64(DBSize)))
-	M1 := 4 * uint64(math.Sqrt(float64(DBSize))*math.Log(float64(DBSize)))
-	M2 := 4 * uint64(math.Log(float64(DBSize)))
-	log.Printf("Q: %d, M1: %d, M2: %d", Q, M1, M2)
+	return Server{
+		DB:        DB,
+		DBSize:    DBSize,
+		ChunkSize: ChunkSize,
+		ChunkNum:  ChunkNum,
+	}
+}
 
-	//Setup Phase
-	//The client first samples the hints
-	primaryHints := make([]LocalHint, M1)
-	backupHints := make([]LocalHint, M2*ChunkNum)
-	for i := uint64(0); i < M1; i++ {
+func (s Server) NewClient() Client {
+	Q := uint64(math.Sqrt(float64(s.DBSize)) * math.Log(float64(s.DBSize)))
+	M1 := 4 * uint64(math.Sqrt(float64(s.DBSize))*math.Log(float64(s.DBSize)))
+	M2 := 4 * uint64(math.Log(float64(s.DBSize)))
+
+	return Client{
+		DBSize:    s.DBSize,
+		ChunkSize: s.ChunkSize,
+		ChunkNum:  s.ChunkNum,
+
+		Q:  Q,
+		M1: M1,
+		M2: M2,
+	}
+}
+
+func (s Server) Query(index uint64) uint64 {
+	return s.DB[index]
+}
+
+type ClientState struct {
+	config          Client
+	rng             *rand.Rand
+	primaryHints    []LocalHint
+	backupHints     []LocalHint
+	localCache      map[uint64]uint64
+	consumedHintNum []uint64
+}
+
+func (c Client) InitializeState(s Server) *ClientState {
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	primaryHints := make([]LocalHint, c.M1)
+	backupHints := make([]LocalHint, c.M2*c.ChunkNum)
+	for i := uint64(0); i < c.M1; i++ {
 		primaryHints[i] = LocalHint{util.RandKey(rng), 0, 0, false}
 	}
-	for i := uint64(0); i < M2*ChunkNum; i++ {
+	for i := uint64(0); i < c.M2*c.ChunkNum; i++ {
 		backupHints[i] = LocalHint{util.RandKey(rng), 0, 0, false}
 	}
-	//The client streamingly downloads the chunks from the server
-	for i := uint64(0); i < ChunkNum; i++ {
+
+	// Initialize all hints using the randomly sampled values from the database
+	for i := uint64(0); i < c.ChunkNum; i++ {
 		// suppose the client receives the i-th chunk, DB[i*ChunkSize:(i+1)*ChunkSize]
-		for j := uint64(0); j < M1; j++ {
-			primaryHints[j].parity ^= DB[Elem(&primaryHints[j], i)]
+		for j := uint64(0); j < c.M1; j++ {
+			index := c.Elem(&primaryHints[j], i)
+			primaryHints[j].parity ^= s.Query(index)
 		}
-		for j := uint64(0); j < M2*ChunkNum; j++ {
-			if j/M2 != i {
-				backupHints[j].parity ^= DB[Elem(&backupHints[j], i)]
+		for j := uint64(0); j < c.M2*c.ChunkNum; j++ {
+			if j/c.M2 != i {
+				index := c.Elem(&backupHints[j], i)
+				backupHints[j].parity ^= s.Query(index)
 			}
 		}
 	}
 
-	//Online Query Phase
 	localCache := make(map[uint64]uint64)
-	consumedHintNum := make([]uint64, ChunkNum)
-	for q := uint64(0); q < Q; q++ {
-		// just do random query for now
-		x := rng.Uint64() % DBSize
+	consumedHintNum := make([]uint64, c.ChunkNum)
 
-		// make sure x is not in the local cache
-		for {
-			if _, ok := localCache[x]; ok == false {
-				break
-			}
-			x = rng.Uint64() % DBSize
+	return &ClientState{
+		config:          c,
+		rng:             rng,
+		primaryHints:    primaryHints,
+		backupHints:     backupHints,
+		localCache:      localCache,
+		consumedHintNum: consumedHintNum,
+	}
+}
+
+type ClientQuery struct {
+	state   *ClientState
+	index   uint64
+	chunkId uint64
+	hitId   uint64
+}
+
+func (c ClientQuery) Prepare() []uint64 {
+	offsetVec := make([]uint64, c.state.config.ChunkNum)
+	for i := uint64(0); i < c.state.config.ChunkNum; i++ {
+		offsetVec[i] = c.state.config.Elem(&c.state.primaryHints[c.hitId], i) % c.state.config.ChunkSize
+	}
+	punctOffsetVec := offsetVec[0:c.chunkId]
+	punctOffsetVec = append(punctOffsetVec, offsetVec[c.chunkId+1:]...)
+
+	return punctOffsetVec
+}
+
+func (c *ClientState) RandomQuery() ClientQuery {
+	x := c.rng.Uint64() % c.config.DBSize
+
+	// make sure x is not in the local cache
+	for {
+		if _, ok := c.localCache[x]; ok == false {
+			break
 		}
+		x = c.rng.Uint64() % c.config.DBSize
+	}
 
-		chunkId := x / ChunkSize
-		hitId := uint64(999999999)
-		for i := uint64(0); i < M1; i++ {
-			if Elem(&primaryHints[i], chunkId) == x {
-				hitId = i
-				break
-			}
-		}
-		if hitId == uint64(999999999) {
-			log.Fatalf("Error: cannot find the hitId")
-		}
-
-		offsetVec := make([]uint64, ChunkNum)
-		for i := uint64(0); i < ChunkNum; i++ {
-			offsetVec[i] = Elem(&primaryHints[hitId], i) % ChunkSize
-		}
-		punctOffsetVec := offsetVec[0:chunkId]
-		punctOffsetVec = append(punctOffsetVec, offsetVec[chunkId+1:]...)
-
-		//send the punctured offset vector to the server and get the parities
-		parities := PossibleParities(punctOffsetVec)
-		answer := parities[chunkId] ^ primaryHints[hitId].parity
-
-		// This verification only happens in this demo experiment.
-		if answer != DB[x] {
-			log.Fatalf("Error: answer is not correct")
-		}
-
-		// update the local cache
-		localCache[x] = answer
-
-		// refresh the hint
-		if consumedHintNum[chunkId] < M2 {
-			primaryHints[hitId] = backupHints[chunkId*M2+consumedHintNum[chunkId]]
-			primaryHints[hitId].isProgrammed = true
-			primaryHints[hitId].programmedPoint = x
-			primaryHints[hitId].parity ^= answer
-			consumedHintNum[chunkId]++
-		} else {
-			log.Fatalf("Not enough backup hints")
+	chunkId := x / c.config.ChunkSize
+	hitId := uint64(999999999)
+	for i := uint64(0); i < c.config.M1; i++ {
+		if c.config.Elem(&c.primaryHints[i], chunkId) == x {
+			hitId = i
+			break
 		}
 	}
-	log.Printf("PIR finished successfully")
+	if hitId == uint64(999999999) {
+		log.Fatalf("Error: cannot find the hitId")
+	}
+
+	return ClientQuery{
+		state:   c,
+		index:   x,
+		chunkId: chunkId,
+		hitId:   hitId,
+	}
+}
+
+func (c *ClientState) RecoverAnswer(clientQuery ClientQuery, serverParities []uint64) uint64 {
+	answer := serverParities[clientQuery.chunkId] ^ c.primaryHints[clientQuery.hitId].parity
+	c.localCache[clientQuery.index] = answer
+
+	if c.consumedHintNum[clientQuery.chunkId] < c.config.M2 {
+		c.primaryHints[clientQuery.hitId] = c.backupHints[clientQuery.chunkId*c.config.M2+c.consumedHintNum[clientQuery.chunkId]]
+		c.primaryHints[clientQuery.hitId].isProgrammed = true
+		c.primaryHints[clientQuery.hitId].programmedPoint = clientQuery.index
+		c.primaryHints[clientQuery.hitId].parity ^= answer
+		c.consumedHintNum[clientQuery.chunkId]++
+	} else {
+		// XXX(caw): what is the proper failure condition here?
+		log.Fatalf("Not enough backup hints")
+	}
+
+	return answer
 }
 
 func main() {
-	DBSize = 10000 // please make sure DBSize is a perfect square
-	PIR()
+	dbSize := uint64(10000)
+	server := NewServer(dbSize)
+	client := server.NewClient()
+	clientState := client.InitializeState(server)
+
+	for q := uint64(0); q < client.Q; q++ {
+		query := clientState.RandomQuery()
+		preparedQuery := query.Prepare()
+		response := server.Process(preparedQuery)
+		answer := clientState.RecoverAnswer(query, response)
+		if answer != server.Query(query.index) {
+			log.Fatalf("Error: answer is not correct")
+		}
+	}
 }
